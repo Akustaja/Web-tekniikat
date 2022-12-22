afa711daa824dd70774e1b4da2e64094ca572485
Aku Nikula <aku.nikula@gmail.com>
2022-12-22 17:00:51 +0200

    UI-functionality task finished

--- UI & JS -tehtävä/js/script.js
+++ UI & JS -tehtävä/js/script.js
@@ -1,1 +1,28 @@
-//Koodia
+//Functionality for the JS-exercise
+
+
+
+
+
+
+
+//Other functionality
+let main = document.querySelectorAll(".main");
+
+//Header icons
+
+let infoIcon = document.querySelector(".info");
+let infoPage = document.getElementById("info");
+infoIcon.addEventListener("click", () => infoPage.classList.toggle("hide"));
+
+let archiveIcon = document.querySelector(".archive");
+archiveIcon.addEventListener("click", () => {
+    infoPage.classList.add("hide");
+    main.forEach(n => n.classList.add("hide"));
+});
+
+let addIcon = document.querySelector(".add");
+addIcon.addEventListener("click", () => {
+    infoPage.classList.add("hide");
+    main.forEach(n => n.classList.remove("hide"));
+})
